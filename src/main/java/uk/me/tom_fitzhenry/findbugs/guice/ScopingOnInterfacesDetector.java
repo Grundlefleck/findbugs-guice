d85fe1158f3f8f1ae937d2fdf02fa09ffea56282
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2011-07-29 21:34:23 +0100

    Whitespace fixes.

--- src/main/java/uk/me/tom_fitzhenry/findbugs/guice/ScopingOnInterfacesDetector.java
+++ src/main/java/uk/me/tom_fitzhenry/findbugs/guice/ScopingOnInterfacesDetector.java
@@ -11,44 +11,45 @@ import edu.umd.cs.findbugs.classfile.IAnalysisCache;
 import edu.umd.cs.findbugs.classfile.analysis.AnnotationValue;
 
 public class ScopingOnInterfacesDetector implements Detector2 {
-	private final BugReporter bugReporter;
-	
-	public ScopingOnInterfacesDetector(BugReporter bugReporter) {
-		this.bugReporter = bugReporter;
-	}
-	
-	@Override
-	public void visitClass(ClassDescriptor classDescriptor) {
-		final XClass classInfo = classDescriptionToXClass(classDescriptor);
-        
-        if(classInfo.isInterface()) {
-        	for(AnnotationValue annotation : classInfo.getAnnotations()) {
-        		if (annotation.getAnnotationClass().getClassName().contains("Singleton")) {
-        			bugReporter.reportBug(new BugInstance(this, "GUICE_SCOPE_ON_ANNOTATION", NORMAL_PRIORITY).addClass(classDescriptor));
-        		}
-        	}
+    private final BugReporter bugReporter;
+
+    public ScopingOnInterfacesDetector(BugReporter bugReporter) {
+        this.bugReporter = bugReporter;
+    }
+
+    @Override
+    public void visitClass(ClassDescriptor classDescriptor) {
+        final XClass classInfo = classDescriptionToXClass(classDescriptor);
+
+        if (classInfo.isInterface()) {
+            for (AnnotationValue annotation : classInfo.getAnnotations()) {
+                if (annotation.getAnnotationClass().getClassName().contains("Singleton")) {
+                    bugReporter.reportBug(new BugInstance(this, "GUICE_SCOPE_ON_ANNOTATION",
+                            NORMAL_PRIORITY).addClass(classDescriptor));
+                }
+            }
         }
-	}
+    }
 
-	private XClass classDescriptionToXClass(ClassDescriptor classDescriptor) {
-		final IAnalysisCache analysisCache = Global.getAnalysisCache();
+    private XClass classDescriptionToXClass(ClassDescriptor classDescriptor) {
+        final IAnalysisCache analysisCache = Global.getAnalysisCache();
 
-		XClass classInfo = null;
+        XClass classInfo = null;
         try {
-			classInfo = analysisCache.getClassAnalysis(XClass.class, classDescriptor);
-		} catch (CheckedAnalysisException e) {
-			bugReporter.logError("Error when converting a classDescriptor, " + classDescriptor + ", "
-					           + "to an XClass.", e);
-		}
-		return classInfo;
-	}
+            classInfo = analysisCache.getClassAnalysis(XClass.class, classDescriptor);
+        } catch (CheckedAnalysisException e) {
+            bugReporter.logError("Error when converting a classDescriptor, " + classDescriptor
+                    + ", " + "to an XClass.", e);
+        }
+        return classInfo;
+    }
 
-	@Override
-	public void finishPass() {
-		// TODO Auto-generated method stub
-	}
+    @Override
+    public void finishPass() {
+        // TODO Auto-generated method stub
+    }
 
-	@Override
+    @Override
     public String getDetectorClassName() {
         return getClass().getName();
     }
